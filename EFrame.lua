c1e286763117f9deb65cd93db232f694dcf07b92
macklunky <60305591+macklunky@users.noreply.github.com>
2022-05-26 03:45:06 -0700

    Update EFrame.lua

--- EFrame.lua
+++ EFrame.lua
@@ -134,7 +134,7 @@ EFrame.new = function(parent)
 			canRender = EFrame.Parent._rendered
 		end
 		EFrame._rendered = canRender and EFrame.Visible or false
-		EFrame._properties.--[[VisibleValue = EFrame._rendered]]Visible = EFrame._rendered
+		EFrame._properties.VisibleValue = EFrame._rendered--Visible = EFrame._rendered
 		for index, child in next, EFrame._children do
 			child:_renderUpdate()
 		end
@@ -202,7 +202,7 @@ EFrame.new = function(parent)
 			if index == "Parent" and value ~= nil then
 				print(typeof(value) == "table")
 				print(rawget(value, "_EGUITAG"))
-				print("tests")
+				print("testing")
 				if typeof(value) ~= "table" or typeof(value) == "table" and rawget(value, "_EGUITAG") == nil then
 					error("Attempt to assign invalid object as Parent", 2)
 				end
