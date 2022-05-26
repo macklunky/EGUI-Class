50664f43ed37cf5c5fb5fed5a4943cf90358ba34
macklunky <60305591+macklunky@users.noreply.github.com>
2022-05-25 19:47:07 -0700

    Create lib.lua

---
+++ lib.lua
@@ -0,0 +1,143 @@
+local InputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+
+local Signal = require(script.Parent.Signal)
+
+local function FlipTransparency(num)
+	return 1 - num
+end
+
+local EGUI = {}
+
+EGUI.new = function()
+	local EGUI = {
+		_properties = {
+			Enabled = {
+				Value = true
+			}
+		},
+		_methods = {},
+		_events = {},
+		_connections = {},
+		_destroyed = false
+	}
+
+	EGUI.FindFirstChild = function(tab, name)
+		if not tab then
+			error("Expected ':' not '.' calling member function FindFirstChild", 2)
+		end
+		if not name then
+			error("Argument 1 missing or nil", 2)
+		end
+		if typeof(name) ~= "string" and typeof(name) ~= "number" then
+			error("Unable to cast " .. typeof(name) .. " to string", 2)
+		end
+		name = tostring(name)
+
+		if rawget(EGUI, "_destroyed") == false and EGUI._children[name] then
+			return EGUI._children[name]
+		end
+	end
+
+	EGUI.GetChildren = function(tab, name)
+		if not tab then
+			error("Expected ':' not '.' calling member function GetChildren", 2)
+		end
+		if rawget(EGUI, "_destroyed") == false then
+			local children = {}
+			for index, child in next, EGUI._children do
+				children[index] = child
+			end
+			return children
+		end
+	end
+
+	EGUI.Destroy = function(tab)
+		if not tab then
+			error("Expected ':' not '.' calling member function Destroy", 2)
+		end
+		for event, signal in next, tab._events do
+			signal:DisconnectAll()
+		end
+		tab._children = {}
+	end
+
+	setmetatable(EGUI, {
+		__index = function(tab, index)
+			if typeof(index) ~= "string" and typeof(index) ~= "number" then
+				error(index .. " is not a valid member of EFrame", 2)
+			end
+			if tab._events[index] ~= nil then
+				return tab._events[index]
+			elseif tab._properties[index] ~= nil then
+				return tab._properties[index].Value
+			elseif tab._methods[index] ~= nil then
+				return tab._methods[index]
+			elseif tab:FindFirstChild(index) then
+				return tab:FindFirstChild(index)
+			else
+				error(index .. " is not a valid member of EFrame", 2)
+			end
+		end,
+
+		__newindex = function(tab, index, value)
+			if typeof(index) ~= "string" and typeof(index) ~= "number" then
+				error(index .. " is not a valid member of EFrame", 2)
+			end
+			if tab._properties[index] then
+				tab._properties[index] = value
+			else
+				error(index .. " is not a valid member of EFrame", 2)
+			end
+		end,
+	})
+
+	task.spawn(function()
+		local lastMousePosition = InputService:GetMouseLocation()
+		while rawget(EGUI, "_destroyed") == false do
+			--update gui mouseenter mouseleave mousemove events
+			local newMousePosition = InputService:GetMouseLocation()
+			if newMousePosition ~= lastMousePosition then
+
+
+				--do stuff
+				--update gui rendering
+				
+
+				lastMousePosition = newMousePosition
+			end
+			RunService.RenderStepped:Wait()
+		end
+	end)
+
+	local changedConnection
+	changedConnection = InputService.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseWheel then
+			--update gui scroll events with input.Position
+		end
+	end)
+
+	local beganConnection
+	beganConnection = InputService.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			--update mouse1down events with input.Position
+		elseif input.UserInputType == Enum.UserInputType.MouseButton2 then
+			--update mouse2down events with input.Position
+		elseif input.UserInputType == Enum.UserInputType.MouseButton3 then
+			--update mouse3down events with input.Position
+		end
+	end)
+
+	local endedConnection
+	endedConnection = InputService.InputEnded:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			--update mouse1up events with input.Position
+		elseif input.UserInputType == Enum.UserInputType.MouseButton2 then
+			--update mouse2up events with input.Position
+		elseif input.UserInputType == Enum.UserInputType.MouseButton3 then
+			--update mouse3up events with input.Position
+		end
+	end)
+end
+
+return EGUI
